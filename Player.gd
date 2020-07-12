8036421d6ae5fed1314951c50854136815f7323d
NotHawthorne <nothawthorne@airmail.cc>
2020-07-11 21:05:25 -0700

    LFS integration, assets and tiles, player sprites and animations for controller.

--- Player.gd
+++ Player.gd
@@ -5,19 +5,36 @@ const GRAV = 20
 const STEP = 800
 const JUMP = -20
 var velocity = Vector2.UP
+var state = "falling"
+var left_flip = false
+var jumping = false
 
 func _physics_process(delta):
 	velocity.y += GRAV
 	if Input.is_action_pressed('ui_right'):
 		velocity.x = STEP
+		if is_on_floor():
+			$AnimatedSprite.animation = "run"
+		left_flip = false
 	elif Input.is_action_pressed('ui_left'):
 		velocity.x = -STEP
+		if is_on_floor():
+			$AnimatedSprite.animation = "run"
+		left_flip = true
 	else:
 		velocity.x = 0
+		if is_on_floor():
+			$AnimatedSprite.animation = "idle"
+	$AnimatedSprite.set_flip_h(left_flip)
 
 	if is_on_floor():
 		if Input.is_action_just_pressed('ui_up'):
 			velocity.y = -STEP
+			$AnimatedSprite.animation = "jump_start"
+			jumping = true
+		elif jumping == true:
+			$AnimatedSprite.animation = "jump_end"
+			jumping = false
 #	get_input()
 	velocity = move_and_slide(velocity, UP)
 	pass
--- SimplexNoiseTileMap.gd
+++ SimplexNoiseTileMap.gd
@@ -4,6 +4,15 @@ var noise = OpenSimplexNoise.new()
 var grid = []
 const size = 128
 # Configure
+func _get_subtile_coord(id):
+	print(id)
+	if (id == 0):
+		return Vector2(0, 0)
+	var rect = tile_set.tile_get_region(id)
+	var x = (id % int(rect.size.x))
+	var y = (id / int(rect.size.y))
+	return Vector2(x, y)
+
 func _ready():
 	noise.seed = 13
 	noise.lacunarity = 2.0
@@ -15,10 +24,10 @@ func _ready():
 		grid[x] = []
 		grid[x].resize(size + 1)
 		for y in size:
-			grid[x][y] = -1 if noise.get_noise_2d(x, y) > 0 else 0
+			grid[x][y] = 0 if noise.get_noise_2d(x, y) > 0 else 1
 	for x in size:
 		for y in size:
-			set_cell(x, y, grid[x][y])
+			set_cell(x, y, grid[x][y], false, false, false, _get_subtile_coord(grid[x][y]))
 	#Another way to access noise values is to precompute a noise image:
 	# This creates a 512x512 image filled with simplex noise (using the currently set parameters)
 	#var noise_image = noise.get_image(512, 512)
